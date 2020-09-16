fc91f16f477130804c082d1a3ca8c3e21a611dd6
Raphael Maciel Vilela <raph.mv.cbjr@gmail.com>
2020-09-16 11:07:30 -0300

    Stream

---
+++ src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+
+		//criar uma stream apartir da lista
+		//Stream<Integer> st1 = list.stream();
+		
+		//utilizando map pra aplicar uma função a cada elemento da stream.Nesse cada multiplicando por 10
+		Stream<Integer> st1 = list.stream().map(x-> x*10);
+		
+		//imprimindo a stream na tela
+		System.out.println(Arrays.toString(st1.toArray()));
+		
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
+		
+		Stream<Integer> st3 = Stream.iterate(0, x-> x+2);
+		//limit - so sera imprimido na tela nesse caso 10 elementos.
+		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		
+		//fibonacci
+		Stream<Long> st4 = Stream.iterate(new Long[]{0L, 1L}, p-> new Long[] {p[1], p[0]+p[1]}).map(p->p[0]);
+		System.out.println(Arrays.toString(st4.limit(10).toArray()));
+		
+	}
+
+}
